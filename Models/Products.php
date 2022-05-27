b7cb588d1d925464862f690f4672114c3b3ae1f9
MyUsername <romamargine@gmail.com>
2022-05-27 16:27:49 +0200

    Add Fix class Product

--- Models/Products.php
+++ Models/Products.php
@@ -1,17 +1,23 @@
 <?php
 
-class Products {
+trait Products {
 
-    public $species; 
+    public $species;
     public $product;
+    public $name;
+    public $brand;
+    public $description;
     public $price;
 
 
-    public function __construct(String $species, String $price, String $product )
+    public function __construct(String $species, String $product, string $name, String $brand, String $description, String $price)
     {
         $this-> species = $species;
-        $this-> price = $price;
         $this-> product = $product;
+        $this-> name = $name;
+        $this-> brand = $brand;
+        $this-> description = $description;
+        $this-> price = $price;
     }
     
 }
